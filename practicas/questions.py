44a3f08df42f4d9b95f816b09a4b5eb15c2376f9
Euge-taller2025 <ecattelanpedrazzini@gmail.com>
2025-03-21 22:44:11 -0300

    Calculo de puntaje

--- practicas/questions.py
+++ practicas/questions.py
@@ -24,6 +24,8 @@ answers = [
 ]
 # Índice de la respuesta correcta para cada pregunta, en el mismo orden que las preguntas
 correct_answers_index = [1, 2, 0, 3, 1]
+puntaje=0;
+
 # El usuario deberá contestar 3 preguntas
 for _ in range(3):
     # Se selecciona una pregunta aleatoria
@@ -45,7 +47,10 @@ for _ in range(3):
                 sys.exit (1) #termina el proograma
             if user_answer ==  correct_answers_index[question_index]:
                 print("¡Correcto!")
+                puntaje +=1
                 break #es la sentencia para salir del bucle en caso de condicion correcta
+            else:
+                puntaje= puntaje - 0.5
         except ValueError:
             print ('Respuesta no valida')
             sys.exit (1) #termina el proograma
@@ -58,3 +63,5 @@ for _ in range(3):
         print(answers[question_index][correct_answers_index[question_index]])
      # Se imprime un blanco al final de la pregunta
     print()
+
+print(f"Has obtenido {puntaje} puntos")
