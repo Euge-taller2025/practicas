d9d73b43cc73bd3bbee708f7a4bf598b248e8982
Euge-taller2025 <ecattelanpedrazzini@gmail.com>
2025-03-21 23:33:12 -0300

    Modificacion de la lista de tuplas para generar una pregunta aleatoria

--- practicas/questions.py
+++ practicas/questions.py
@@ -26,14 +26,14 @@ answers = [
 correct_answers_index = [1, 2, 0, 3, 1]
 puntaje=0
 
-# El usuario deberá contestar 3 preguntas
-for _ in range(3):
-    # Se selecciona una pregunta aleatoria
-    question_index = random.randint(0, len(questions) - 1)
+#combino las tres listas en una lista de tuplas, para cada pregunta, tengo las respuestas y el indice de la corrrecta
+#zip es la tupla que luego se convierte en una lista para poder acceder con el random
+questions_to_ask = random.choices(list(zip(questions,answers, correct_answers_index)), k=3)
 
-    # Se muestra la pregunta y las respuestas posibles
-    print(questions[question_index])
-    for i, answer in enumerate(answers[question_index]):
+# El usuario deberá contestar 3 preguntas las selecciono al azar de la lista
+for question, possible_answers, correct_answer_index in questions_to_ask:
+    print (question)
+    for i, answer in enumerate(possible_answers):
         print(f"{i + 1}. {answer}")
 
  # El usuario tiene 2 intentos para responder correctamente
@@ -46,11 +46,11 @@ for _ in range(3):
             else:
                 user_answer= int(user_imput)-1
                 # Se verifica si la respuesta es correcta
-                if user_answer<0 or user_answer>= len(answers[question_index]): 
+                if user_answer<0 or user_answer>= len(possible_answers): 
                 #si lo que ingresa es menor a 0 o mayor o igual a 3 da error 
                     print ('Respuesta no valida')
                     sys.exit (1) #termina el programa
-                elif user_answer ==  correct_answers_index[question_index]:
+                elif user_answer ==  correct_answer_index:
                     print("¡Correcto!")
                     puntaje +=1
                     break #es la sentencia para salir del bucle en caso de condicion correcta
@@ -62,7 +62,7 @@ for _ in range(3):
         # Si el usuario no responde correctamente después de 2 intentos,
         # se muestra la respuesta correcta
         print("Incorrecto. La respuesta correcta es:")
-        print(answers[question_index][correct_answers_index[question_index]])
+        print(possible_answers[correct_answer_index])
      # Se imprime un blanco al final de la pregunta
     print()
 
